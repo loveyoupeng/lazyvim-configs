6d95756ac3d299088734bf066c6beee6cb207f80
loveyoupeng <loveyoupeng@gmail.com>
2025-09-16 23:24:06 +0800

    clang-format

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,6 +7,7 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
+        null_ls.builtins.formatting.clang_format,
         require("none-ls.formatting.ruff"),
         require("none-ls.formatting.ruff_format"),
         require("none-ls.formatting.trim_newlines"),
